4c61cf9226820cf44c8ef6b60f1ec8b50ec46ae3
KanishkKalyani <kanishkkalyani@gmail.com>
2020-12-18 11:52:11 +0530

    Time Tracker server using GraphQL

---
+++ index.js
@@ -0,0 +1,82 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { v4: uuid } = require('uuid');
+
+const typeDefs = gql`
+   type Tracker {
+      id: ID!
+      name: String!
+      start: String!
+      end: String!
+      tags: [String!]
+   }
+
+   type Query {
+      tracker: [Tracker]
+   }
+
+   type Mutation {
+
+      addTrack(
+         name: String,
+      ): Tracker
+
+      editTrack(
+         id: ID!,
+         name: String,
+         start: String,
+         end: String,
+         tags: [String]
+      ): Tracker
+
+      deleteTrack(id: ID!): DeleteResponse
+   }
+
+   type DeleteResponse {
+      ok: Boolean!
+   }
+`;
+
+const tracker = {};
+
+const addTracker = track => {
+   const id = uuid();
+   const start = "";
+   const end = "";
+   const tags = ["Temp Tag"];
+   return tracker[id] = { ...track, id, start, end, tags };
+ };
+
+ addTracker({
+   name: 'Test Track',
+});
+
+const resolvers = {
+   Query: {
+     tracker: () => Object.values(tracker),
+   },
+   Mutation: {
+      addTrack: async (parent, track, context) => {
+         return addTracker(track);
+      },
+      editTrack: async (parent, { id, ...track }, context) => {
+         tracker[id] = {
+            ...tracker[id],
+            ...track,
+          };
+
+          return tracker[id];
+      },
+      deleteTrack: async (parent, { id }, context) => {
+         const ok = Boolean(tracker[id]);
+         delete tracker[id];
+
+         return { ok };
+      }
+   },
+ };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
